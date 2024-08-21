1eef5c0134bfbb1196465717fd5dc1b9b4740b2b
immanuel-savvy <immanuelsavvy@gmail.com>
2024-08-21 09:38:31 +0100

    1724229511817683304

--- Index.js
+++ Index.js
@@ -1,56 +1,55 @@
 import Manager from "./Objects/Manager";
 import create_server from "./utils/create_server";
 
-let manager = new Manager();
+// let manager = new Manager();
 
-let initiator = manager.initiate({
-  on_framed: () =>
-    initiator.assembler.run(
-      `@/datastore/main
+// let initiator = manager.initiate({
+//   on_framed: () =>
+//     initiator.assembler.run(
+//       `@/datastore/main
 
-      >object
-      >keys []
-      >entries {}
-      >size 0
-      
-      ./set
-      >key
-      >key_static getter key "static_value"
-      >value
-      >exists getter ../entries key
-      jmp_not_void :proceed
-      >../size add ../size 1
-      >../keys setter ../keys -1 key
-      
-      :proceed 
-      >../entries setter ../entries key_static value
-      ;
-      
-      ./get
-      >key
-      >value getter ../entries key
-      ;
-      
-      ./remove
-      >key
-      >../entries delete ../entries key
-      >../keys delete ../keys key
-      >../size sub ../size 1
-      ;
-      
-      ./get_keys
-      >output ../keys 
-      ;
-      
+//       >object
+//       >keys []
+//       >entries {}
+//       >size 0
 
-    ;`,
-      {
-        cb: (blks) => {
-          console.log(blks, blks.length, "did");
-        },
-      }
-    ),
-});
+//       ./set
+//       >key
+//       >key_static getter key "static_value"
+//       >value
+//       >exists getter ../entries key
+//       jmp_not_void :proceed
+//       >../size add ../size 1
+//       >../keys setter ../keys -1 key
+
+//       :proceed
+//       >../entries setter ../entries key_static value
+//       ;
+
+//       ./get
+//       >key
+//       >value getter ../entries key
+//       ;
+
+//       ./remove
+//       >key
+//       >../entries delete ../entries key
+//       >../keys delete ../keys key
+//       >../size sub ../size 1
+//       ;
+
+//       ./get_keys
+//       >output ../keys
+//       ;
+
+//     ;`,
+//       {
+//         cb: (blks) => {
+//           console.log(blks, blks.length, "did");
+//         },
+//       }
+//     ),
+// });
 
 // create_server(null, { manager });
 
